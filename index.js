680151820570d4aac924ba60e547b60cc94bc3ef
Gissur Orange <gorange-@am-d3.msk.21-school.ru>
2021-03-20 15:40:55 +0300

    3.20 add Handlebars to project

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const { request } = require('express');
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.status(200);
-   res.sendFile(path.join(__dirname, 'views', 'index.html')); 
+   res.render('index'); 
 });
 
 app.get('/about', (req, res) => {
-    res.status(200);
-    res.sendFile(path.join(__dirname, 'views', 'about.html')); 
+    res.render('about');
  });
 
+
+// conf hbs for express
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+// регистрация движка
+app.engine('hbs', hbs.engine)
+// добавление движка
+app.set('view engine', 'hbs')
+// регистрация папки под страницы
+app.set('views', 'views');
+
+
+
+ // configurate PORT 
 const PORT = process.env.PORT || 3000;
+
+
 
 app.listen(3000 , () => {
     console.log(`Server is running on port: ${PORT}`)
