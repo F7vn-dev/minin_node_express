72ff4d6f7ee7b46b6fc5a550efb3964fefd5150f
F7vn <favndev@gmail.com>
2021-03-23 13:00:30 +0300

    3.26 Create model

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,6 @@
 const {Router} = require("express");
+const Course = require('../models/course')
+
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -8,9 +10,11 @@ router.get('/', (req, res) => {
     });
  });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     console.log(req.body)
 
+    const course = new Course(req.body.title, req.body.cost, req.body.img);
+    await course.save();
     res.redirect('/courses')
 })
 
