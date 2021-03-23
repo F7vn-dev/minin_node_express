e06dd072cea549ee2ae8f05834e2c5f941b80f8d
F7vn <favndev@gmail.com>
2021-03-23 15:16:57 +0300

    2.29 Dynamic parameters

--- routes/courses.js
+++ routes/courses.js
@@ -3,13 +3,21 @@ const router = Router();
 const Course = require('../models/course')
 
 router.get('/', async (req, res) => {
-
     const courses = await Course.getAll();
     console.log(courses);
     res.render('courses', {
         title: "Courses",
         isCourses: true,
         courses: courses,
+    });
+})
+
+router.get('/:id', async (req, res) => {
+    const course = await Course.getById(req.params.id);
+    res.render('course', {
+        layout: "empty",
+        title: `Курс ${course.title}`,
+        course 
     });
 })
 
